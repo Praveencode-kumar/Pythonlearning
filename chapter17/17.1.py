3be6d7978316b201635b34c3595a71113e9247dc
Praveencode-kumar <76721437+Praveencode-kumar@users.noreply.github.com>
2021-01-18 05:58:13 -0800

    Add files via upload

---
+++ chapter17/17.1.py
@@ -0,0 +1,14 @@
+#Exercise 17.1
+
+class Time(object):
+    def time_to_int(self):
+        minutes = time.hour * 60 + time.minute
+        seconds = minutes * 60 + time.second
+        return seconds
+
+time = Time()
+time.hour = 11
+time.minute = 59
+time.second = 30
+
+print time.time_to_int()
